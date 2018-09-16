1a9a7dea85431f2ba26f16333382c6d16afc5a03
Tomek_M <tomasz.milaszewski@gmail.com>
2018-09-16 17:04:30 +0200

    59 floors borders done with small hack

--- game/game.js
+++ game/game.js
@@ -1,19 +1,6 @@
 //główny kontener planszy
 var boardContainer = document.querySelector('#board-wrapper');
 
-//cztery poziomy planszy, poziom floor-0 - najniższy
-// var floor3 = document.querySelector('.floor-3');
-// var floor2 = document.querySelector('.floor-2');
-// var floor1 = document.querySelector('.floor-1');
-// var floor0 = document.querySelector('.floor-0');
-
-//drzwi wejściowe (punkt startowy - poziom floor-1) i drzwi wyjściowe (punkt końcowy - poziom floor-3)
-// var startDoors = document.querySelector('.start-doors');
-// var endDoors = document.querySelector('.end-doors');
-
-//podział poziomów na części
-// var floorDivision = 10; //każdy poziom dzielimy na tyle mniejszych
-
 //przykładowa plansza: _-empty cell,s-startDoors,e-endDoors,l-drabina,d-diamond 
 var boardData = [
     '_e__d___d_',
@@ -22,22 +9,26 @@ var boardData = [
     '_d____l__s',
 ]
 
+//wygenerowanie grida na elementy plaszy, nadanie mu klasy .grid i przypięcie do boardContainera
 var grid = document.createElement('div');
 grid.classList.add('grid');
 boardContainer.appendChild(grid);
 
+//wygenerowanie i przypięcie do grida wierszy wg schematu planszy
 for (var j = 0; j < boardData.length; j += 1) {
     var row = document.createElement('div');
     row.classList.add('gridRow');
+    row.style.borderBottom = '1px solid black'; //hack na nakładanie się borderów
 
-    grid.appendChild(row);
+    grid.appendChild(row); //przypięcie wygenerowanego wiersza do grida
 
     var rowData = boardData[j];
     var size = rowData.length;
 
+    //stworzenie komórek (cells) w obrębie wiersza (row) wg schematu planszy
     for (var i = 0; i < size; i += 1) {
         var cell = document.createElement('div');
-        cell.classList.add('emptyCell');
+        cell.classList.add('emptyCell'); //nadanie komórce (cell) klasy domyślnej .emptyCell
         if (rowData[i] === 's') {
             cell.classList.add('start-doors');
         }
@@ -50,6 +41,6 @@ for (var j = 0; j < boardData.length; j += 1) {
         if (rowData[i] === 'd') {
             cell.classList.add('diamond');
         }
-        row.appendChild(cell);
+        row.appendChild(cell); //przypięcie pojedynczej komórki (cell) do wiersza (row)
     }
 }
