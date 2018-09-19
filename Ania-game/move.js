97a0836b1590bcd33f2d95b7853c711696f5bc6c
ankaziele <ankaziele@gmail.com>
2018-09-19 16:21:44 +0200

    add move left/move right with margin

--- Ania-game/move.js
+++ Ania-game/move.js
@@ -3,24 +3,45 @@ var position = 0;
 var velocity = 0;
 
 window.addEventListener ('keydown', function (event){
-    if (event.code === 'Space'){
+    if (event.code === 'ArrowRight'){
         velocity = 1;
     }
 
 })
 
 window.addEventListener ('keyup', function (event){
-    if (event.code === 'Space'){
+    if (event.code === 'ArrowRight'){
         velocity = 0;
     }
 
 })
+
 
 setInterval(function (){
 position += velocity;
 hugo.style.marginLeft = position + 'px'
 
 }, 15)
+
+
+window.addEventListener ('keydown', function (event){
+    if (event.code === 'ArrowLeft'){
+        velocity = -1;
+    }
+
+})
+
+window.addEventListener ('keyup', function (event){
+    if (event.code === 'ArrowLeft'){
+        velocity = 0;
+    }
+
+})
+
+setInterval(function (){
+    position += velocity;
+    hugo.style.marginLeft = position + 'px'
+    }, 15)
  
 // window.addEventListener ('keydown', function (event){
 //     if (event.code === 'arrowLeft'){
