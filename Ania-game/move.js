85fbeb8980ecbd5c85d4371faff4b2b3b84fd6a6
ankaziele <ankaziele@gmail.com>
2018-09-20 17:01:07 +0200

    moving and stop and board

--- Ania-game/move.js
+++ Ania-game/move.js
@@ -1,6 +1,5 @@
-var hugo = document.querySelector ('#hugo');
-var position = 0;
-var velocity = 0;
+// var position = 0;
+// var velocity = 0;
 
 // *moving with MARGIN
 // window.addEventListener ('keydown', function (event){
@@ -56,13 +55,19 @@ var level = document.getElementById ('level');
 
 // document.onkeydown = anim;
 
-var hugoLeft = 0;
-var hugoTop = 0;
+//wyznaczamy pozycje poczatkowe hugo wzgledem rozmiarow planszy
+var hugoLeft = level.clientWidth - hugo.clientWidth;
+var hugoTop = level.clientHeight - hugo.clientHeight;
 
+hugo.style.left = hugoLeft+'px'; //tu dodajemy px do stringa bo wartosc musi byc w pixelach
+hugo.style.top =  hugoTop +'px';
 
 function anim(e){
     if(e.keyCode==39){
         hugoLeft+= 2;
+        if (hugoLeft < 0){
+            hugoLeft = 0;
+        }
         hugo.style.left = hugoLeft + 'px';
 
 
@@ -71,19 +76,29 @@ function anim(e){
 
     if(e.keyCode== 37){
         hugoLeft-=2;
+        if (hugoLeft < 0){
+            hugoLeft = 0;
+        }
         hugo.style.left = hugoLeft + 'px';
     }
 
     if(e.keyCode== 40){
         hugoTop +=2;
+        if (hugoTop > level.clientHeight-hugo.clientHeight){
+            hugoTop = level.clientHeight-hugo.clientHeight;
+        } 
         hugo.style.top = hugoTop + 'px';
     }
 
     if(e.keyCode== 38){
         hugoTop -=2;
+        
+        if (hugoTop < 0){
+            hugoTop = 0;
+        } 
         hugo.style.top = hugoTop + 'px';
     }
 }
 
-document.onkeydown = anim;
-
+// document.onkeydown = anim; //skortowy zapis przypisania f-cji do eventu keydown - to samo moze zrobic addevent listener?
+document.addEventListener ('keydown', anim);
