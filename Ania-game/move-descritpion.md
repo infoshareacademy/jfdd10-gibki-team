0ce7a0f6feeecd9e85c304b4a104d9988c105e15
ankaziele <ankaziele@gmail.com>
2018-09-21 15:52:32 +0200

    add levels

---
+++ Ania-game/move-descritpion.md
@@ -0,0 +1,15 @@
+
+
+1. podzial na pietra
+
+c
+robie divy ktore wypelniajarowno przestrzen
+
+2. w divach wylosowac pozycje drabin
+create element drabiny math random
+zapisac pozycje drabin do zmiennej pomocniczej
+
+zeszyt tyl
+
+
+3. jak mam drabiny to zablokowac drabiny zeby tylko isc w gore
--- Ania-game/move.js
+++ Ania-game/move.js
@@ -1,3 +1,28 @@
+var level = document.getElementById('level');
+
+// level.appendChild(makeLevel(500, 500));
+
+// function makeLevel(width, height) {
+//     var table = document.createElement('table');
+//     var tbody = document.createElement('tbody');
+//     var tr, i;
+
+//     for (i = 0; i < height; i += 1) {
+//         tr = document.createElement('tr');
+//         if (i % 2 === 0) {
+//             tr.classList.add('blueLevel')
+//         }
+//         tbody.appendChild(tr);
+//     }
+//     table.appendChild(tbody);
+
+//     return table;
+
+
+// }
+
+
+
 // var position = 0;
 // var velocity = 0;
 
@@ -42,11 +67,11 @@
 //     position += velocity;
 //     hugo.style.marginLeft = position + 'px'
 //     }, 15)
- 
+
 // Moving with POSITION
 
-var hugo = document.getElementById ('hugo');
-var level = document.getElementById ('level');
+var hugo = document.getElementById('hugo');
+
 
 // funkcja ponizej sprawdza kod klawiszy - live preview - wcisnij przyiski i poznaj kod
 // function anim(e){
@@ -59,46 +84,48 @@ var level = document.getElementById ('level');
 var hugoLeft = level.clientWidth - hugo.clientWidth;
 var hugoTop = level.clientHeight - hugo.clientHeight;
 
-hugo.style.left = hugoLeft+'px'; //tu dodajemy px do stringa bo wartosc musi byc w pixelach
-hugo.style.top =  hugoTop +'px';
+hugo.style.left = hugoLeft + 'px'; //tu dodajemy px do stringa bo wartosc musi byc w pixelach
+hugo.style.top = hugoTop + 'px';
 
-function anim(e){
-    if(e.keyCode==39){
-        hugoLeft+= 2;
-        if (hugoLeft > level.clientWidth-hugo.clientWidth){
-            hugoLeft = level.clientWidth-hugo.clientWidth;
+function anim(e) {
+    if (e.keyCode == 39) {
+        hugoLeft += 2;
+        if (hugoLeft > level.clientWidth - hugo.clientWidth) {
+            hugoLeft = level.clientWidth - hugo.clientWidth;
         }
         hugo.style.left = hugoLeft + 'px';
 
 
     }
-
-
-    if(e.keyCode== 37){
-        hugoLeft-=2;
-        if (hugoLeft < 0){
+    if (e.keyCode == 37) {
+        hugoLeft -= 2;
+        if (hugoLeft < 0) {
             hugoLeft = 0;
         }
         hugo.style.left = hugoLeft + 'px';
     }
 
-    if(e.keyCode== 40){
-        hugoTop +=2;
-        if (hugoTop > level.clientHeight-hugo.clientHeight){
-            hugoTop = level.clientHeight-hugo.clientHeight;
-        } 
+    if (e.keyCode == 40) {
+        hugoTop += 2;
+        if (hugoTop > level.clientHeight - hugo.clientHeight) {
+            hugoTop = level.clientHeight - hugo.clientHeight;
+        }
         hugo.style.top = hugoTop + 'px';
     }
 
-    if(e.keyCode== 38){
-        hugoTop -=2;
-        
-        if (hugoTop < 0){
+    if (e.keyCode == 38) {
+        hugoTop -= 2;
+
+        if (hugoTop < 0) {
             hugoTop = 0;
-        } 
+        }
         hugo.style.top = hugoTop + 'px';
     }
 }
 
 // document.onkeydown = anim; //skortowy zapis przypisania f-cji do eventu keydown - to samo moze zrobic addevent listener?
-document.addEventListener ('keydown', anim);
+document.addEventListener('keydown', anim);
+
+
+
+
